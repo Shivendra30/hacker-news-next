942be99274ee58352a9e87467f51a7876521f9fc
Shivendra <30shivam@gmail.com>
2020-03-23 08:17:23 +0530

    change next config

--- next.config.js
+++ next.config.js
@@ -5,29 +5,36 @@ const withPurgeCss = require("next-purgecss");
 const glob = require("glob");
 const path = require("path");
 
-const nextConfig = {
-  // generateInDevMode: true
-};
-
 // const nextConfig = {
-//   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-//     // Note: we provide webpack above so you should not `require` it
-//     // Perform customizations to webpack config
-//     // Important: return the modified config
-//     config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-//     config.plugins.push(
-//       new PurgecssPlugin({
-//         paths: glob.sync(`${path.join(__dirname)}/**/*`, { nodir: true })
-//       })
-//     );
-//     return config;
-//   },
-//   webpackDevMiddleware: config => {
-//     // Perform customizations to webpack dev middleware config
-//     // Important: return the modified config
-//     return config;
-//   }
 //   // generateInDevMode: true
 // };
+
+const nextConfig = {
+  target: "serverless",
+  transformManifest: manifest => ["/"].concat(manifest), // add the homepage to the cache
+  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+  // turn on the SW in dev mode so that we can actually test it
+  generateInDevMode: true,
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "https-calls",
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
+  }
+};
 
 module.exports = withOffline(withCss(withPurgeCss(nextConfig)));
