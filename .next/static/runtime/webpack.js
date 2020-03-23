121d949581509b5c19fcca796e410368348f802d
Shivendra <30shivam@gmail.com>
2020-03-23 08:20:29 +0530

    Remove server and change package.json scripts

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f236b1a959dbaa3c50e6";
+/******/ 	var hotCurrentHash = "32ac21eac87e280d3fb7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
